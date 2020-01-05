9891a821a33957a369ea454f5803e1947e1d2a42
arnie b <arnold.bwaila@gmail.com>
2020-01-05 23:57:00 +0000

    Done.

--- lib/author.rb
+++ lib/author.rb
@@ -8,4 +8,17 @@ class Author
   def posts 
     Post.all.select {|posts| posts.author = self}
   end
+  
+  def add_post(post)
+    post.author = self
+  end
+  
+  def add_post_by_title(title)
+    post = Post.new(title)
+    add_post(post)
+  end
+  
+  def self.post_count
+    Post.all.count
+  end
 end
